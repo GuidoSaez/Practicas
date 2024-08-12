545e78d706f58667a27d4e0bc279fdea97abd906
Guido Saez <saezguido1@gmail.com>
2024-08-12 15:16:32 -0300

    fizzbuzz terminado

---
+++ eloquent-JS/capitulo-02/fizzbuzz.js
@@ -0,0 +1,11 @@
+for (let numero = 1; numero <= 100; numero++) {
+  if (numero % 5 === 0 && numero % 3 === 0) {
+    console.log('fizzbuzz');
+  } else if (numero % 5 === 0) {
+    console.log('buzz');
+  } else if (numero % 3 === 0) {
+    console.log('fizz');
+  } else {
+    console.log(numero);
+  }
+}
